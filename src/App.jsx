430f3d662dbf12cb730a1781984b1fbabdf0e7b4
AShirreffs <ashirreffs58@gmail.com>
2026-07-21 16:34:04 -0700

    Set up React Vite project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <h1>Welcome!</h1>
+        <p>Your projects will appear here.</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
